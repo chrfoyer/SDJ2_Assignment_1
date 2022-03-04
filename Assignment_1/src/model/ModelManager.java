b0921756fb7cca4d722be7c213eb215f0da7919b
MartyBobo <martin.rosendahl4@gmail.com>
2022-03-04 11:31:03 +0100

    updated modemanager and changed addlistenter(this)

--- Assignment_1/src/model/ModelManager.java
+++ Assignment_1/src/model/ModelManager.java
@@ -12,16 +12,40 @@ public class ModelManager implements Model
   public ModelManager()
   {
     property = new PropertyChangeSupport(this);
+
   }
 
-  @Override public void addListener(String propertyName,
-      PropertyChangeListener listener)
+  @Override public void updateTemperature(Double temperature)
+  {
+
+  }
+
+  @Override public Heater getHeater()
+  {
+    return null;
+  }
+
+  @Override public Thermometer getThermometer()
+  {
+    return null;
+  }
+
+  @Override public void increase()
+  {
+
+  }
+
+  @Override public void decrease()
+  {
+
+  }
+
+  @Override public void addListener(PropertyChangeListener listener)
   {
     property.addPropertyChangeListener(listener);
   }
 
-  @Override public void removeListener(String propertyName,
-      PropertyChangeListener listener)
+  @Override public void removeListener(PropertyChangeListener listener)
   {
     property.removePropertyChangeListener(listener);
   }
